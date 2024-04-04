f29f2747d1db40a4cd92c3d47c7c231e8705691d
Stephan Granzow <stephan_granzow@web.de>
2024-04-04 18:03:57 +0200

    done day 2

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,56 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.currentQuestionIndex = 0;
+    this.correctAnswers = 0;
+  }
+  //   filterQuestionsByDifficulty(difficulty) {
+  //     this.questions.filter((e) => {});
+  //   }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    this.questions.sort(() => Math.random() - 0.5);
+  }
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    const currentQuestion = this.getQuestion();
+    if (answer === currentQuestion.answer) {
+      this.correctAnswers += 1;
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty !== 1 && difficulty !== 2 && difficulty !== 3) {
+      return;
+    } else {
+      this.questions = this.questions.filter((e) => {
+        return e.difficulty === difficulty;
+      });
+    }
+  }
+  averageDifficulty() {
+    const difficulty = this.questions.reduce((acc, question) => {
+      const sum = acc + question.difficulty;
+      return sum;
+    }, 0);
+    return difficulty / this.questions.length;
+  }
 }
